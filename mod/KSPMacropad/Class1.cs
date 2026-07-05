bc1357f9ad46184ffe9ca184435ad546ea6b7362
Montgomery Brown <techmanminty@gmail.com>
2026-07-05 02:47:25 -0400

    beginning mod dev

---
+++ mod/KSPMacropad/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO.Ports;
+using UnityEngine;
+
+namespace KSPMacropad
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class KSPMacropad : MonoBehaviour
+    {
+        private SerialPort serial;
+
+        void Start()
+        {
+            serial = new SerialPort("COM3", 115200);
+            serial.Open();
+        }
+
+        void Update()
+        {
+            if (FlightGlobals.ActiveVessel == null) return;
+
+            Vessel v = FlightGlobals.ActiveVessel;
+        }
+
+        void OnDestroy()
+        {
+            if (serial != null && serial.IsOpen)
+                serial.Close();
+        }
+    }
+}
